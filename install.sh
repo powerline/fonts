109a62b705212255f18bfb7840e0b8e4f6e54627
Tony Thayer <tmt@tonythayer.com>
2014-11-04 08:54:18 -0800

    Update install.sh
    Further testing shows that the find command fails for GNU find under Ubuntu 11.10 and EL6.

--- install.sh
+++ install.sh
@@ -3,14 +3,14 @@
 # Set source and target directories
 powerline_fonts_dir=$( cd "$( dirname "$0" )" && pwd )
 
+find_command="find $powerline_fonts_dir \( -name '*.[o,t]tf' -or -name '*.pcf.gz' \) -type f -print0"
+
 if [[ `uname` == 'Darwin' ]]; then
   # MacOS
   font_dir="$HOME/Library/Fonts"
-  find_command="find $powerline_fonts_dir \( -name '*.[o,t]tf' -or -name '*.pcf.gz' \) -type f -print0"
 else
   # Linux
   font_dir="$HOME/.fonts"
-  find_command="find $powerline_fonts_dir -name '*.[o,t]tf' -o -name '*.pcf.gz' -type f -print0"
   mkdir -p $font_dir
 fi
 
