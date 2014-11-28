1614e260a7c180ef9fe4b5a2f0057ad87abc2b7c
Sebastian Busch <mail@sebastian-bus.ch>
2014-11-28 21:17:41 +0100

    allow spaces in powerline fonts dir name

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # Set source and target directories
 powerline_fonts_dir=$( cd "$( dirname "$0" )" && pwd )
 
-find_command="find $powerline_fonts_dir \( -name '*.[o,t]tf' -or -name '*.pcf.gz' \) -type f -print0"
+find_command="find \"$powerline_fonts_dir\" \( -name '*.[o,t]tf' -or -name '*.pcf.gz' \) -type f -print0"
 
 if [[ `uname` == 'Darwin' ]]; then
   # MacOS
@@ -15,7 +15,7 @@ else
 fi
 
 # Copy all fonts to user fonts directory
-eval $find_command | xargs -0 -I % cp % $font_dir/
+eval $find_command | xargs -0 -I % cp "%" "$font_dir/"
 
 # Reset font cache on Linux
 if [[ -n `which fc-cache` ]]; then
